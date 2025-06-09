7446d717a3482eea12b5ed41191f7b7a98f05c0e
Michael Ramirez <michaelrp2000@gmail.com>
2025-06-09 15:35:25 -0500

    V 02 frontend

---
+++ frontend/programa.js
@@ -0,0 +1,24 @@
+var map = L.map('map').setView([4.629580, -74.065737], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([4.629580, -74.065737]).addTo(map);
+
+//Manejar evento
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+map.on('click', onMapClick);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
